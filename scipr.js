65614767dc1a38fc9ea5484245a040b454687340
Abdiel <abdiel.dsem@gmail.com>
2023-07-15 17:23:46 -0400

    first commit

---
+++ scipr.js
@@ -0,0 +1,44 @@
+
+// let num1 = 2;
+// let num2 = 1; 
+// let result = num1 + num2;
+// console.log ( result);
+
+// if(result >= 1 && result <= 3 ){
+//     console.log("your note is low");
+// }
+// else if(result >= 4 && result <= 5)
+// {
+//     console.log("your note is good");
+// }
+// else {
+//    console.log("did not the test")
+// }
+// ---------------------------------------------------------------------------------------
+// let num1 = 2;
+// let num2 = 1; 
+// let result = num1 + num2;
+// console.log ( result);
+
+// if(result >= 1 && result <= 3 ){
+//     alert("your note is low");
+// }
+// else if(result >= 4 && result <= 5)
+// {
+//     alert("your note is good");
+// }
+// else {
+//    alert("did not the test")
+// }
+// ---------------------------------------------------------------
+// function substrac(no1, no2, no3, no4){
+//     alert(no1 - no2 - no3 - no4)
+// }
+// substrac(1, 2, 3, 4)
+
+// -------------------------------------------------------------------------
+function divide(no1, no2){
+    alert(no1/no2)
+    alert(no1%no2)
+}
+divide(10, 5)
